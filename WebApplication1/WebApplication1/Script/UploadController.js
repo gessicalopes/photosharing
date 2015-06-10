9aeabf7a60c365eb378351f7625b0062c4a4a756
unknown <u031220@Fiplan-029341.prodebti.intranet>
2015-06-10 18:30:52 -0300

    commit cliente PhotoSharing

---
+++ WebApplication1/WebApplication1/Script/UploadController.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Script/Controllers')
+        .controller('UploadController', uploadController);
+    
+
+    function uploadController($scope, $http, $upload) {
+        var me = this;
+        this.arquivo = null;
+        me.iniciar = iniciar;
+        //iniciar();
+   
+
+         $scope.results = [];
+         function iniciar() {
+             $scope.showSpinner('Carregando');
+             $http.get('http://serviceupload.azurewebsites.net/api/upload').success(function (data) {
+                   $scope.results = data;
+                   $scope.hideSpinner();
+               }).error(function (error) {
+
+             });
+         };
+         
+         this.selecionarArquivo = function (file) {
+             this.arquivo = file[0];
+         }
+
+         this.uploadFileToUrl = function () {
+                 $upload.upload({
+                 url: 'http://serviceupload.azurewebsites.net/api/upload', // webapi url
+                 method: 'POST',
+                 file: this.arquivo
+             }).success(function () {
+                 alert("Imagem Inserida com Sucesso");
+                 me.iniciar();
+             })
+            .error(function () {
+            });
+        }
+    }
+
+})();
